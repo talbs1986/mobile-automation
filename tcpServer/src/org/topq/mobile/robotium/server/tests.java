fe04b5083e1b08e15bd324fef53c95ef23729a92
talbs1986 <talbs1986@gmail.com>
2013-01-14 19:58:08 +0200

    merged robotium server apk (leftovers)

---
+++ tcpServer/src/org/topq/mobile/robotium/server/tests.java
@@ -0,0 +1,73 @@
+package org.topq.mobile.robotium.server;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.topq.mobile.tcp.impl.TcpExecutorServer;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class tests extends ActivityInstrumentationTestCase2 {
+	private static final String TAG = "ANDROID CLIENT";
+
+	private static final String TARGET_PACKAGE_ID = "com.gettaxi";
+	private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.gettaxi.android.activities.login.LoadingActivity";
+	private static Class launcherActivityClass;
+
+	static {
+
+		try {
+			launcherActivityClass = Class.forName(LAUNCHER_ACTIVITY_FULL_CLASSNAME);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public tests() throws ClassNotFoundException {
+		super(TARGET_PACKAGE_ID, launcherActivityClass);
+	}
+
+	private Solo solo;
+
+	@Override
+	protected void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	public void testLogin() throws IOException {
+
+		File file = new File("/data/app.txt");
+		InputStream in = new FileInputStream(file);
+		InputStreamReader input = new InputStreamReader(in);
+		BufferedReader buffreader = new BufferedReader(input);
+
+		String line = " ";
+		String res = "";
+		while ((line = buffreader.readLine()) != null) {
+			String[] command = line.split(";");
+			if (0 == command[0].compareTo("enterText")) {
+				solo.enterText(Integer.parseInt(command[1]), command[2]);
+			} else if (0 == command[0].compareTo("clickOnButton")) {
+				solo.clickOnButton(command[1]);
+			}
+		}
+		in.close();
+
+	}
+
+	public void testConnection() {
+		Log.d(TAG, "About to create connection");
+		TcpExecutorServer server = TcpExecutorServer.getInstance(4321);
+		Thread t = new Thread(server);
+		t.start();
+		Log.d(TAG, "Connection is up");
+	}
+
+}
