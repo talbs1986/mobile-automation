9f75b85fcee4f5224dc7ad66a76603a60f38879c
talbs1986 <talbs1986@gmail.com>
2013-01-14 08:54:12 +0200

    functional test for the example app

--- mobile-client/src/test/java/org/topq/mobile/client/test/Main.java
+++ mobile-client/src/test/java/org/topq/mobile/client/test/Main.java
@@ -10,7 +10,13 @@ public class Main {
 			MobileClientInterface x = new MobileClient(null,false,true);
 			x.launch("com.tal.example.loginapp.LoginActivity");
 			x.enterText(0, "tal@tal.com");
-			
+			x.enterText(1, "1234567");
+			x.clickOnButtonWithText("Sign in or register");
+			Thread.sleep(1000 * 5);
+			x.clickOnButtonWithText("Ok");
+			x.clickOnButtonWithText("Sign in or register");
+			Thread.sleep(1000 * 5);
+			x.clickOnButtonWithText("Ok");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
