574730e9931019a02058a74168d69e63c6b17d1a
talbe <talbe@TALBE-DESK.nice.com>
2013-02-05 17:18:53 +0200

    removed android support jar removed logs from command parser

--- common-mobile/src/main/java/org/topq/mobile/common/server/utils/CommandParser.java
+++ common-mobile/src/main/java/org/topq/mobile/common/server/utils/CommandParser.java
@@ -4,7 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.util.Log;
+//import android.util.Log;
 
 public class CommandParser {
 
@@ -12,17 +12,17 @@ public class CommandParser {
 	private  JSONObject command;
 
 	public CommandParser(final String data) throws JSONException{
-		Log.d(TAG, "Pars command: " + data);
+//		Log.d(TAG, "Pars command: " + data);
 		 command = new JSONObject(data);
 	}
 
 	public String getCommand() throws JSONException {
-		Log.d(TAG, "command is:" + command.getString("Command"));
+//		Log.d(TAG, "command is:" + command.getString("Command"));
 		return command.getString("Command");
 	}
 
 	public JSONArray getArguments() throws JSONException {
-		Log.d(TAG, "Params are:" +  command.get("Params"));
+//		Log.d(TAG, "Params are:" +  command.get("Params"));
 		return (JSONArray) command.get("Params");
 	}
 
